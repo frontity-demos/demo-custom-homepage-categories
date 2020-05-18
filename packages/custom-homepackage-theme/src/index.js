bcd07ec028811e09e7f0de2dbd63b9b7dcf3555e
JuanMa Garrido <juanma.garrido@gmail.com>
2020-05-18 18:40:42 +0200

    init way

--- packages/custom-homepackage-theme/src/index.js
+++ packages/custom-homepackage-theme/src/index.js
@@ -12,16 +12,23 @@ export default {
   },
   actions: {
     theme: {
-      init: async ({ state, actions }) => {
-        if (state.router.link === "/") {
+      beforeSSR: () => {
+        console.log('beforeSSR...')
+      }
+    },
+    source: {
+      init: ({ state, actions }) => {
+        console.log('init...')
+        console.log(state.router.link)
+        // if (state.router.link === "/") {
           
-          console.log(state.router.link)
-          console.log('getting data from beforeSSR...')
-          await Promise.all(
-            Object.values(categoriesWidgetsHome)
-              .map(category => actions.source.fetch(`/category/${category}/`))
-          )
-        }
+        //   console.log(state.router.link)
+        //   console.log('getting data from init...')
+        //   await Promise.all(
+        //     Object.values(categoriesWidgetsHome)
+        //       .map(category => actions.source.fetch(`/category/${category}/`))
+        //   )
+        // }
       }
     }
   }
